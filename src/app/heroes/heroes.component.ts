aa2d1d8c3729eeee36a329ba9468a9ea21bf26f1
Washington Silva <washington.s@escolar.ifrn.edu.br>
2020-10-31 14:52:24 -0300

    Display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+
+import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
 
 @Component({
@@ -7,6 +9,8 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
 
   hero: Hero = {
     id: 1,
@@ -18,4 +22,9 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(this.selectedHero);
+  }
 }
